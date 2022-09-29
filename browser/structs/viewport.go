eea97fa22037377e7f610f4eba6adae3af94b143
qqq000qqq@aliyun.com <xqq1258460261>
2022-09-29 14:28:50 +0800

    20220929

--- browser/structs/viewport.go
+++ browser/structs/viewport.go
@@ -1,6 +1,12 @@
 package structs
 
-import ui_structs "ui/structs"
+import (
+	"layout"
+	"log"
+	profiler_structs "profiler/structs"
+	renderer_structs "renderer/structs"
+	ui_structs "ui/structs"
+)
 
 type Viewport struct {
 	Frame     *ui_structs.Frame
@@ -23,4 +29,49 @@ func CreateViewport(webBrowser *WebBrowser) *Viewport {
 		View:      view,
 		ScrollBar: scrollBar,
 	}
+}
+
+func GetViewportRenderer(webBrowser *WebBrowser) func(*ui_structs.CanvasWidget) {
+	return func(canvas *ui_structs.CanvasWidget) {
+		go func() {
+			profiler := webBrowser.Profiler
+			document := webBrowser.CurrentDocument
+			ui := webBrowser.UI
+			headBar := ui.HeadBar
+			viewport := ui.Viewport
+
+			profiler.Start("render")
+			ctxBounds := canvas.GetContext().GetImage().Bounds()
+			drawingContext := renderer_structs.CreateContext(ctxBounds.Max.X, ctxBounds.Max.Y)
+
+			err := layout.LayoutDocument(drawingContext, document)
+			if err != nil {
+				log.Fatal("render", "Can't render page: "+err.Error())
+			}
+
+			canvas.SetContext(drawingContext)
+			canvas.RequestReflow()
+			profiler.Stop("render")
+
+			statusLabel := headBar.StatusLabel
+			statusLabel.SetContent(createStatusLabel(profiler))
+			statusLabel.RequestReflow()
+			canvas.RequestReflow()
+
+			body, err := webBrowser.CurrentDocument.DOM.FindChildByName("body")
+			if err != nil {
+				log.Fatal("render", "can't find body element: "+err.Error())
+				return
+			}
+
+			scrollBar := viewport.ScrollBar
+			scrollBar.SetScrollerSize(body.RenderBox.Height)
+			scrollBar.RequestReflow()
+		}()
+	}
+}
+
+func createStatusLabel(perf *profiler_structs.Profiler) string {
+	return "Loaded; " +
+		"Render took: " + perf.GetProfile("render").GetElapsedTime().String() + "; "
 }
--- browser/structs/web_browser_ui.go
+++ browser/structs/web_browser_ui.go
@@ -1,10 +1,6 @@
 package structs
 
 import (
-	"layout"
-	"log"
-	profiler_structs "profiler/structs"
-	renderer_structs "renderer/structs"
 	ui_structs "ui/structs"
 )
 
@@ -31,49 +27,4 @@ func CreateWebBrowserUI(webBrowser *WebBrowser) *WebBrowserUI {
 		HeadBar:  headBar,
 		Viewport: viewport,
 	}
-}
-
-func GetViewportRenderer(webBrowser *WebBrowser) func(*ui_structs.CanvasWidget) {
-	return func(canvas *ui_structs.CanvasWidget) {
-		go func() {
-			profiler := webBrowser.Profiler
-			document := webBrowser.CurrentDocument
-			ui := webBrowser.UI
-			headBar := ui.HeadBar
-			viewport := ui.Viewport
-
-			profiler.Start("render")
-			ctxBounds := canvas.GetContext().GetImage().Bounds()
-			drawingContext := renderer_structs.CreateContext(ctxBounds.Max.X, ctxBounds.Max.Y)
-
-			err := layout.LayoutDocument(drawingContext, document)
-			if err != nil {
-				log.Fatal("render", "Can't render page: "+err.Error())
-			}
-
-			canvas.SetContext(drawingContext)
-			canvas.RequestReflow()
-			profiler.Stop("render")
-
-			statusLabel := headBar.StatusLabel
-			statusLabel.SetContent(createStatusLabel(profiler))
-			statusLabel.RequestReflow()
-			canvas.RequestReflow()
-
-			body, err := webBrowser.CurrentDocument.DOM.FindChildByName("body")
-			if err != nil {
-				log.Fatal("render", "can't find body element: "+err.Error())
-				return
-			}
-
-			scrollBar := viewport.ScrollBar
-			scrollBar.SetScrollerSize(body.RenderBox.Height)
-			scrollBar.RequestReflow()
-		}()
-	}
-}
-
-func createStatusLabel(perf *profiler_structs.Profiler) string {
-	return "Loaded; " +
-		"Render took: " + perf.GetProfile("render").GetElapsedTime().String() + "; "
 }
